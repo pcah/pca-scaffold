706a4f991bb743d0cc6b50ce06e45f78a3ce2b92
lhaze <lhaze@lhaze.name>
2022-05-13 00:43:04 +0200

    [fix, tests] Fixed scaffold's linting tests

--- {{cookiecutter.project_slug}}/tests/test_cli.py
+++ {{cookiecutter.project_slug}}/tests/test_cli.py
@@ -1,4 +1,4 @@
-{% if cookiecutter.command_line_interface|lower == 'click' -%}
+{% if cookiecutter.command_line_interface|lower == "click" -%}
 from click.testing import CliRunner
 
 from {{ cookiecutter.package_qualified_name }} import cli
@@ -9,8 +9,8 @@ def test_command_line_interface():
     runner = CliRunner()
     result = runner.invoke(cli.main)
     assert result.exit_code == 0
-    assert '{{ cookiecutter.project_slug }}' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
+    assert "{{ cookiecutter.project_slug }}" in result.output
+    help_result = runner.invoke(cli.main, ["--help"])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert "--help" in help_result.output
 {%- endif %}
